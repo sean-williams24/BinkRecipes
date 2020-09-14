60c8e5b3954932626ff27b785086851fdfc616a2
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-09-14 13:01:21 +0100

    feat: format category cell. create models for meals and recipes

--- BinkRecipes/Models/MealDB.swift
+++ BinkRecipes/Models/MealDB.swift
@@ -11,6 +11,7 @@ import Foundation
 // MARK: - MealDB
 struct MealDB: Codable {
     let categories: [Category]
+    let meals: [Meal]
 }
 
 // MARK: - Category
@@ -18,4 +19,44 @@ struct Category: Codable {
     let idCategory, strCategory: String
     let strCategoryThumb: String
     let strCategoryDescription: String
+}
+
+// MARK: - Meal
+struct Meal: Codable {
+    let strMeal: String
+    let strMealThumb: String
+    let idMeal: String
+}
+
+// MARK: - Recipe
+struct Recipe: Codable {
+    let idMeal: String
+    let strMeal: String
+    let strDrinkAlternate: String
+    let strCategory: String
+    let strArea: String
+    let strInstructions: String
+    let strMealThumb: String
+    let strTags: String
+    let strYouTube: String
+    let strIngredient1: String
+    let strIngredient2: String
+    let strIngredient3: String
+    let strIngredient4: String
+    let strIngredient5: String
+    let strIngredient6: String
+    let strIngredient7: String
+    let strIngredient8: String
+    let strIngredient9: String
+    let strIngredient10: String
+    let strMeasure1: String
+    let strMeasure2: String
+    let strMeasure3: String
+    let strMeasure4: String
+    let strMeasure5: String
+    let strMeasure6: String
+    let strMeasure7: String
+    let strMeasure8: String
+    let strMeasure9: String
+    let strMeasure10: String
 }
--- BinkRecipes/Views/CategoryCollectionViewCell.swift
+++ BinkRecipes/Views/CategoryCollectionViewCell.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Sean Williams. All rights reserved.
 //
 
+import SDWebImage
 import UIKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
@@ -15,9 +16,24 @@ class CategoryCollectionViewCell: UICollectionViewCell {
     
     var category: Category! {
         didSet {
-            textLabel.text = category.strCategory
-            imageView.image = UIImage()
+            textLabel.text = category.strCategory.uppercased()
+            imageView.sd_setImage(with: URL(string: category.strCategoryThumb), placeholderImage: UIImage(named: "bink"))
         }
     }
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        letterSpacing(label: textLabel, value: 5.0)
+        
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 0.6
+        layer.cornerRadius = 3
+        
+    }
+    
+    func letterSpacing(label: UILabel, value: Double) {
+        let attributedString = NSMutableAttributedString(string: label.text!)
+        attributedString.addAttribute(NSAttributedString.Key.kern, value: CGFloat(value), range: NSRange(location: 0, length: attributedString.length))
+        label.attributedText = attributedString
+    }
 }

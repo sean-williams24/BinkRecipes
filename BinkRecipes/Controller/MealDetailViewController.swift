9b5da2ffaf23d7783cbfcc6af10fc3f4324a0e30
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-09-15 08:27:48 +0100

    feat: Setup Meal Details VC in IB and connect outlets

--- BinkRecipes/Controller/MealDetailViewController.swift
+++ BinkRecipes/Controller/MealDetailViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import YoutubePlayer_in_WKWebView
 
 class MealDetailViewController: UIViewController {
     
     // MARK: - Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var ingredientsLabel: UILabel!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var youTubeView: WKYTPlayerView!
+    
     
     
     // MARK: - Properties
